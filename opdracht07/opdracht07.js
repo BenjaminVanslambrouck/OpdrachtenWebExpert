339880d041d5a7973e2a98573d5b5d54d70602b6
BenjaminVanslambrouck <70214110+BenjaminVanslambrouck@users.noreply.github.com>
2022-09-21 17:21:59 +0200

    aanmaken opdracht  en 7 en maken van oefeningen

---
+++ opdracht07/opdracht07.js
@@ -0,0 +1,58 @@
+function ElementAanmaken() {
+    let nieuweHoofding = document.createElement("h2");
+    let nieuweHoofdingInfo = document.createTextNode("Populaire elektrische fietsen");
+
+    nieuweHoofding.appendChild(nieuweHoofdingInfo);
+
+    document.getElementById("main").appendChild(nieuweHoofding);
+
+
+
+    let nieuweKnop = document.createElement("button");
+    let nieuweKnopTekst = document.createTextNode("Gemiddelde prijs");
+
+    nieuweKnop.appendChild(nieuweKnopTekst);
+
+    document.getElementById("main").appendChild(nieuweKnop);
+
+
+
+    let tweedeNieuweKnop = document.createElement("button");
+    let tweedeNieuweKnopTekst = document.createTextNode("Top 3");
+
+    tweedeNieuweKnop.appendChild(tweedeNieuweKnopTekst);
+
+    document.getElementById("main").appendChild(tweedeNieuweKnop);
+
+
+    let legeDiv = document.createElement('div');
+    legeDiv.setAttribute("id", "Content")
+
+    document.getElementById("main").appendChild(legeDiv);
+
+}
+ElementAanmaken();
+
+function gemiddeldePrijs() {
+
+}
+gemiddeldePrijs();
+let fiets = [
+    {
+        merk:"Kalkhoff",
+        model:"Endeavour 3.B Move",
+        prijs:2499,
+        score:8.7
+    },
+    {
+        merk: "Maranzo",
+        model: "Botarelli 24 kk",
+        prijs: 1799,
+        score: 7.5
+    }
+] ;
+console.log(fiets);
+
+
+
+
