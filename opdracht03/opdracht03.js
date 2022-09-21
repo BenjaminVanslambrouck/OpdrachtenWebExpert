70d4b0400f74ef5fe06d134a6265647dc4365c37
BenjaminVanslambrouck <70214110+BenjaminVanslambrouck@users.noreply.github.com>
2022-09-21 10:31:59 +0200

    aanpassingen opdracht 03 & aanmaken opdracht 4,5 en deze gemaakt

--- opdracht03/opdracht03.js
+++ opdracht03/opdracht03.js
@@ -1,4 +1,4 @@
-const zin = ["Dit is mijn eerste zin", "Dit is mijn tweede zin", "Dit is mijn derde zin"];
+const zin = ["Dit is mijn eerste zin", "Dit is mijn tweede zin", "Dit is mijn derde zin", "Dit is mijn vierde zin"];
 
 zin.forEach((zinnen) =>{
     console.log(zinnen);
@@ -8,7 +8,7 @@ alert("De lengte van de zin is " + zin.length);
 
 for (let i = 0; i < zin.length; i++){
     if (zin[i] === "Dit is mijn tweede zin"){
-        zin.splice(i,1);
+        zin.splice(i,2);
     }
 }
 console.log(zin);
---
+++ opdracht05/opdracht05.js
@@ -0,0 +1,5 @@
+let fiets = {merk:"Kalkhoff", model:"Endeavour 3.B Move", prijs:2499, score:8.7};
+
+if (fiets.prijs < 2500){
+    console.log(fiets);
+}
