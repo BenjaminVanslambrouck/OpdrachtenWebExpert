9b7b18ee1e350fabbb501e7103595ff08325c045
BenjaminVanslambrouck <70214110+BenjaminVanslambrouck@users.noreply.github.com>
2022-09-21 09:03:18 +0200

    opdracht 2 aanpassingen

--- opdracht02/opdracht02.js
+++ opdracht02/opdracht02.js
@@ -1,7 +1,15 @@
 let prijs = parseFloat(prompt("geliever een inkoopprijs te geven!"));
 let btw = parseInt(prompt("gelieve een btw-tarief te geven!"));
+let btwgetal = prijs / 100 * btw;
+let uitkomst = prijs + btwgetal;
 
-if (btw === 0){
+if (uitkomst === NaN){
+    alert("Geen cijfer");
+}else {
+    alert(uitkomst);
+}
+
+/*if (btw === 0){
 
 }
 else if(btw === 6){
@@ -15,4 +23,4 @@ else if(btw === 21){
 }
 else {
     alert('er is geen juiste btw gegeven!');
-}
+}*/
